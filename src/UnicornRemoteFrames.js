6f0e3ec388f008f76cfd9dd6358b3303d05c6194
Fernando Via Canel <fernando.canel@klarna.com>
2017-10-02 16:00:28 +0200

    Unicorn!

---
+++ src/UnicornRemoteFrames.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+// import { type } from 'ramda'
+import { render } from 'react-dom'
+
+let renderInRemote = () => {}
+let removeFromRemove = () => {}
+
+class RemoteFrames extends Component {
+  constructor() {
+    super()
+    this.state = {
+      stack: [<p>Placeholder</p>],
+    }
+
+    renderInRemote = jsx => {
+      const alreadyHere = this.state.stack.find(({ type }) => type === jsx.type)
+      this.setState({ toRender: jsx })
+
+      if (alreadyHere == null) {
+        this.setState(({ stack }) => ({
+          stack: [...stack, jsx],
+        }))
+      }
+    }
+
+    removeFromRemove = jsx => {
+      this.setState(({ stack }) => ({
+        stack: stack.filter(({ type }) => type !== jsx.type),
+      }))
+    }
+  }
+
+  componentDidUpdate() {
+    console.log(this.state)
+  }
+
+  render() {
+    return this.state.stack[this.state.stack.length - 1]
+  }
+}
+
+const targetDomElement = document.createElement('div')
+targetDomElement.setAttribute('id', 'unicorn-remote-frames-target-domm-element')
+document.body.appendChild(targetDomElement)
+
+render(<RemoteFrames />, targetDomElement)
+
+class UnicornRemoteFrame extends Component {
+  constructor(props) {
+    super(props)
+
+    renderInRemote(props.children)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    renderInRemote(nextProps.children)
+  }
+
+  componentWillUnmount() {
+    removeFromRemove(this.props.children)
+  }
+
+  render() {
+    return false
+  }
+}
+
+export default UnicornRemoteFrame
