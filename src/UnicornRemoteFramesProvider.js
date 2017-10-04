718b0b12c615fd7e12a55e8f515d0a1154b53d03
Fernando Via Canel <fernando.canel@klarna.com>
2017-10-04 11:52:43 +0200

    Cleanup cleanup cleanup

--- src/UnicornRemoteFramesProvider.js
+++ src/UnicornRemoteFramesProvider.js
@@ -7,7 +7,6 @@ class UnicornRemoteFramesProvider extends Component {
   constructor(props) {
     super(props)
 
-    // Queue calls and flush when available
     this.queue = []
 
     this.state = {
